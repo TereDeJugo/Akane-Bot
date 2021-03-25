1fd008f288261e292a9baf4d1a10f641820114d2
Tere De Jugo <daniel12onii@gmail.com>
2021-03-25 16:47:15 +0000

    Update Jumbo 2

--- src/commands/utility/jumbo.js
+++ src/commands/utility/jumbo.js
@@ -1,23 +1,27 @@
 const Discord = require("discord.js")
 
 module.exports = {
-  name: "jumbo",
-  alias: ["e"],
-  description: "Ve una imagen completa de un emote",
-  usage: "jumbo <emote>",
-  category: "Utilidad",
-  run: async (client, message, args) => {
-    
-    let emoji = message.guild.emojis.cache.find(x => x.name === args[0].split(":")[1])
-    
-    if (!emoji) {
-        return message.channel.send("Debes colocar un emoji valido del servidor")
+    name: "jumbo",
+    alias: ["e"],
+    description: "Ve una imagen completa de un emote",
+    usage: "jumbo <emote>",
+    category: "Utilidad",
+    run: async (client, message, args) => {
+        if (!args[0]) {
+            return message.channel.send("Especificame un emoji del servidor que quieras ver en grande")
+        }
+        
+        let emoji = message.guild.emojis.cache.find(x => x.name === args[0].split(":")[1])
+
+        if (!emoji) {
+            return message.channel.send("Debes colocar un emoji valido del servidor")
+        }
+
+        const embed = new Discord.MessageEmbed()
+            .setColor("RANDOM")
+            .setDescription(`[Imagen Completa](${emoji.url})`)
+            .setImage(emoji.url)
+
+        message.channel.send(embed)
     }
-    
-    const embed = new Discord.MessageEmbed()
-    .setColor("RANDOM")
-    .setDescription(`[Imagen Completa](${emoji.url})`)
-    .setImage(emoji.url)
-    
-    message.channel.send(embed)
-  }}
+}
