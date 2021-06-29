5172f0143c7e29c5ab9ce29c4b0e30bd19d8348d
LaJBel Ms <71136486+Lajbel@users.noreply.github.com>
2021-06-29 15:01:52 +0000

    Update change avatar

--- src/commands/owner/change-avatar.js
+++ src/commands/owner/change-avatar.js
@@ -5,7 +5,7 @@ module.exports = {
     category: "Owner",
     usage: "change-avatar [avatar]",
     run: async (client, message, args) => {
-        if(!message.attachments.first() || message.attachments.first() == undefined) return message.channel.send("Tienes que enviar una imagen para mi nuevo avatar");
+        if(!message.endsWith(".jpg") || !message.attachments.first() || message.attachments.first() == undefined) return message.channel.send("Tienes que enviar una imagen para mi nuevo avatar");
         client.user.setAvatar(message.attachments.first().url).then(() => message.channel.send("Ya he cambiado mi avatar!"))
     }
 }
